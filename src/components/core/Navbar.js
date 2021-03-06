69e436b8856c1bb19392f686a59be98e9dc4826b
unknown <rix.noah@yahoo.com>
2021-03-05 20:07:23 -0600

    Added a timed button that allows the user to reset the database.

--- src/components/core/Navbar.js
+++ src/components/core/Navbar.js
@@ -1,10 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import axios from 'axios';
-import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Button, IconButton, CircularProgress } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { AuthContext } from '../../util/AuthContext';
-import { backendUrl } from '../../util/AuthUtil';
+import { backendUrl, getNewToken } from '../../util/AuthUtil';
+import { RotateLeft } from '@material-ui/icons';
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,11 +22,13 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Navbar() {
 
-  console.log("backendUrl: " + backendUrl);
 
   const classes = useStyles();
 
-  const { setRefreshToken, setAccessToken, username, isAuthed, setIsAuthed } = useContext(AuthContext);
+  const { accessToken, refreshToken, setRefreshToken, setAccessToken, username, isAuthed, setIsAuthed } = useContext(AuthContext);
+
+  const [canResetDataBase, setCanResetDatabase] = useState(true);
+  const [progress, setProgress] = useState(0);
 
   const handleLogout = () => {
     axios({
@@ -38,12 +42,56 @@ export default function Navbar() {
     })
   }
 
+  const handleResetDatabase = () => {
+    axios({
+      method: 'post',
+      url: backendUrl + '/api/post/auth/reset-database',
+      headers: { authorization: "Bearer " + accessToken }
+    }).then((res) => {
+      console.log("It worked!");
+    }).catch(async () => {
+      setAccessToken(await getNewToken(refreshToken));
+    })
+  }
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 10));
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <AppBar style={{ backgroundColor: "#333" }} position="static">
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
           Bug Tracker
-                </Typography>
+        </Typography>
+        {
+          isAuthed &&
+          <>
+            {
+              canResetDataBase ?
+                <IconButton onClick={() => {handleResetDatabase(); setCanResetDatabase(false);}}><RotateLeft style={{ color: "white" }} /></IconButton>
+                :
+                <div style={{ marginRight: "16px" }}>
+                  <CountdownCircleTimer
+                    size={17}
+                    trailStrokeWidth={2}
+                    strokeWidth={2}
+                    isPlaying={!canResetDataBase}
+                    duration={15}
+                    onComplete={() => setCanResetDatabase(true)}
+                    colors={[["#333333", 0.33]]} >
+                  </CountdownCircleTimer>
+                </div>
+            }
+          </>
+        }
+
         <Button to="/dashboard" component={Link} color="inherit">Dashboard</Button>
         {
           isAuthed ?
@@ -51,6 +99,7 @@ export default function Navbar() {
             :
             <Button to="/login" component={Link} color="inherit">Login</Button>
         }
+
       </Toolbar>
     </AppBar>
   );
